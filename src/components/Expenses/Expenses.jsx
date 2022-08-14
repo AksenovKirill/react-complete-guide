8f2e2349fe750323e0102c25314a13295d4d4319
Kirill Aksenov <aksenovkirill@yandex.ru>
2022-08-15 00:10:15 +0300

    made file and folder management

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { ExpenseItem } from "./ExpenseItem";
+import { ExpenseItem } from "./ExpenseItem/ExpenseItem";
+import { ExpensesFilter } from "./ExpensesFilter/ExpensesFilter";
 import { Card } from "../UI/Card";
 import "./Expenses.css";
-import { ExpensesFilter } from "./ExpensesFilter";
 
 export const Expenses = ({ expenses }) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -14,10 +14,11 @@ export const Expenses = ({ expenses }) => {
   const filteredExpenses = expenses.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
+
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} handleChangeFilter={handleChangeFilter} />
-      {expenses.map((item) => (
+      {filteredExpenses.map((item) => (
         <ExpenseItem key={item.id} item={item} />
       ))}
     </Card>
--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ExpenseForm } from "./ExpenseForm";
+import { ExpenseForm } from "../Expenses/ExpenseForm/ExpenseForm";
 import "./NewExpense.css";
 
 export const NewExpense = ({ handleAddExpense }) => {
