e7c95d915bdf508822ccf6c141c78fa595bd207b
Kirill Aksenov <aksenovkirill@yandex.ru>
2022-08-29 23:24:40 +0300

    finished project

--- practice-1/src/App.js
+++ practice-1/src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
-
-import { AddUser } from "./components/User/AddUser";
+import React, { useState } from "react";
+import { AddUser } from "./components/user/add-user/AddUser";
+import { UserList } from "./components/user/user-list/UserList";
 
 function App() {
+  const [userList, setUserList] = useState([]);
+
+  const handleAddNewUser = (name, age) => {
+    setUserList((prevUsersList) => {
+      return [...prevUsersList, { name, age, id: Math.random().toString() }];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser addNewUser={handleAddNewUser} />
+      <UserList users={userList} />
     </div>
   );
 }
