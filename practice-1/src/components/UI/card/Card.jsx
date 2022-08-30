d58d941c76a91e0d0ce1f0b56eb406b5cbcfa253
Kirill Aksenov <aksenovkirill@yandex.ru>
2022-08-30 22:24:44 +0300

    renamed and deleted files

--- practice-1/src/components/UI/card/Card.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import styles from "./Card.module.css";
-
-export const Card = ({ cssClass, children }) => {
-  return <div className={`${styles.card} ${cssClass}`}>{children}</div>;
-};
