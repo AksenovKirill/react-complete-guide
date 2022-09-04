1fad3485b63f4b25af888bb00c34a2b5ca21ea81
Kirill Aksenov <aksenovkirill@yandex.ru>
2022-09-04 13:49:57 +0300

    added useReducer

--- part-10/src/App.js
+++ part-10/src/App.js
@@ -6,12 +6,14 @@ import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
   useEffect(() => {
     const isLog = localStorage.getItem("isLoggedIn");
     if (isLog === "1") {
       setIsLoggedIn(true);
     }
   }, []);
+  
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
