fff4366901f7dafd571886efbf20b1ea1939a107
Kirill Aksenov <aksenovkirill@yandex.ru>
2022-09-11 15:06:29 +0300

    added memo

--- part-12-optimization/src/components/Demo/DemoOutput.js
+++ part-12-optimization/src/components/Demo/DemoOutput.js
@@ -2,6 +2,6 @@ import React, { memo } from "react";
 import { MyParagraph } from "../MyParagraph";
 
 export const DemoOutput = memo((props) => {
-  console.log("DEMO OUTPUT");
+  console.log("DEMO OUTPUT RUNNING");
   return <MyParagraph>{props.show ? "This is new!" : ""}</MyParagraph>;
 });
--- part-12-optimization/src/components/MyParagraph.jsx
+++ part-12-optimization/src/components/MyParagraph.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
 export const MyParagraph = ({ children }) => {
+  console.log("MyParagraph RUNNING");
   return <p>{children}</p>;
 };
