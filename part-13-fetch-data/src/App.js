eb22c488b7a6734ac72e3d98497b52da9091f33f
Kirill Aksenov <aksenovkirill@yandex.ru>
2022-09-12 08:57:49 +0300

    try-catch. conditionals loading

--- part-13-fetch-data/src/App.js
+++ part-13-fetch-data/src/App.js
@@ -3,27 +3,34 @@ import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
-  const [movies, setMovies] = useState(null);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const handleFetchMovies = () => {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const adaptMovies = data?.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            releaseDate: movieData.release_date,
-            description: movieData.opening_crawl,
-          };
-        });
-        setMovies(adaptMovies);
-      })
-      .catch((error) => {
-        throw new Error(error);
+  const handleFetchMovies = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error("Smthg went wrong");
+      }
+
+      const adaptMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          releaseDate: movieData.release_date,
+          description: movieData.opening_crawl,
+        };
       });
+      setMovies(adaptMovies);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false); 
+      setError(error.message);
+    }
   };
 
   return (
@@ -32,7 +39,10 @@ function App() {
         <button onClick={handleFetchMovies}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </>
   );
