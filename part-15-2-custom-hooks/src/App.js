b18ba106792c0fcc456c54903e235ebe592d219f
Kirill Aksenov <aksenovkirill@yandex.ru>
2022-09-15 21:29:59 +0300

    finished part-15

--- part-15-2-custom-hooks/src/App.js
+++ part-15-2-custom-hooks/src/App.js
@@ -1,39 +1,37 @@
-import React, {useEffect, useState, useCallback} from "react";
-import {useHttp} from "./hooks/useHttp";
+import React, { useEffect, useState, useCallback } from "react";
+import { useHttp } from "./hooks/useHttp";
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
-import {BASE_URL} from "./const";
+import { BASE_URL } from "./const";
 
 function App() {
-    const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
-    const {
-        isLoading, error, sendRequest: fetchTasks,
-    } = useHttp();
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
-    useEffect(() => {
-        const adapterDataToTasks = (taskObj) => {
-            const loadedTasks = [];
+  useEffect(() => {
+    const adapterDataToTasks = (taskObj) => {
+      const loadedTasks = [];
 
-            for (const taskKey in taskObj) {
-                loadedTasks.push({id: taskKey, text: taskObj[taskKey].text});
-            }
-            setTasks(loadedTasks);
-        };
-
-        fetchTasks({
-            url: BASE_URL,
-        }, adapterDataToTasks);
-    }, [fetchTasks]);
-
-    const taskAddHandler = (task) => {
-        setTasks((prevTasks) => prevTasks.concat(task));
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+      setTasks(loadedTasks);
     };
 
-    return (<React.Fragment>
-        <NewTask onAddTask={taskAddHandler}/>
-        <Tasks items={tasks} loading={isLoading} error={error} onFetch={fetchTasks}/>
-    </React.Fragment>);
+    fetchTasks({ url: BASE_URL }, adapterDataToTasks);
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks items={tasks} loading={isLoading} error={error} onFetch={fetchTasks} />
+    </React.Fragment>
+  );
 }
 
 export default App;
